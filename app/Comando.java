37701494ead90196f8c3fe5973887f1a0c8bf7d8
João Dias <joaoadn@outlook.com>
2023-11-29 17:41:35 -0300

    ajustes na interface gráfica

--- app/Comando.java
+++ app/Comando.java
@@ -70,4 +70,3 @@ public class Comando
         return (segundaPalavra != null);
     }
 }
-
--- app/DesempenhoJogoGUI.java
+++ app/DesempenhoJogoGUI.java
@@ -2,6 +2,8 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyAdapter;
 
 /**
  * Classe que representa a interface gráfica do desempenho do jogo.
@@ -19,6 +21,8 @@ public class DesempenhoJogoGUI extends JFrame implements InventarioDisciplinasGU
     private JLabel labelTrabalhos = new JLabel();
     private Timer timer;
     private Jogador jogador;
+    private JTextField comandoTextField;
+    private JTextArea infoTextArea;
 
     /**
      * Construtor padrão da classe DesempenhoJogoGUI.
@@ -41,38 +45,63 @@ public class DesempenhoJogoGUI extends JFrame implements InventarioDisciplinasGU
         panel.add(labelTrabalhos);
         panel.add(tempoLabel);
 
-        add(panel);
-
         // Configuração de cores e fontes
-        panel.setBackground(new Color(97, 77, 92)); // Cor de fundo
-        labelTrabalhos.setForeground(Color.WHITE); // Cor do texto
-        tempoLabel.setForeground(Color.WHITE); // Cor do texto
+        panel.setBackground(Color.WHITE); // Cor de fundo
+        labelTrabalhos.setForeground(Color.BLACK); // Cor do texto
+        tempoLabel.setForeground(Color.BLACK); // Cor do texto
+        
+        // Adiciona um container usando BorderLayout
+        Container container = getContentPane();
+        container.setLayout(new BorderLayout());
 
         Font fonte = new Font("Arial", Font.BOLD, 14); // Fonte personalizada
         labelTrabalhos.setFont(fonte);
         tempoLabel.setFont(fonte);
 
-        ImageIcon icon = new ImageIcon("meuQuarto.png"); // Adiciona a capa do jogo, gerada por AI
-        JLabel labelIcone = new JLabel(icon);
+        // Adiciona um JTextArea para imprimir informações à direita
+        infoTextArea = new JTextArea();
+        infoTextArea.setEditable(false);
+        infoTextArea.setBackground(Color.BLACK);
+        infoTextArea.setForeground(Color.WHITE);
+        JScrollPane scrollPane = new JScrollPane(infoTextArea);
+        scrollPane.setPreferredSize(new Dimension(400, 300)); // Ajuste conforme necessário
+        container.add(scrollPane, BorderLayout.EAST);
 
+        ImageIcon icon = new ImageIcon("capa-zephyrion.png"); // Adiciona a capa do jogo, gerada por AI
+        JLabel labelIcone = new JLabel(icon);
         panel.add(labelIcone);
 
+        // Adiciona campo de texto e botão "Enviar" no painel sul
+        JPanel painelSul = new JPanel(new FlowLayout(FlowLayout.CENTER)); // Usando FlowLayout para centralizar os componentes
+        comandoTextField = new JTextField(20);
+        painelSul.add(comandoTextField);
+
+        // Organiza os componentes da interface gráfica
+        container.add(painelSul, BorderLayout.SOUTH);
+        container.add(panel, BorderLayout.CENTER);
+
         // Adiciona botões
-        JButton botaoSair = new JButton("Sair");
-
-        // Adiciona os botões a um painel
-        JPanel painelBotoes = new JPanel();
-        painelBotoes.add(botaoSair);
-
-        // Adiciona o painel de botões à interface
-        add(painelBotoes, BorderLayout.SOUTH);
+        JButton botaoEnviar = new JButton("Enviar");
+        painelSul.add(botaoEnviar);
+        JButton botaoAjuda = new JButton("Ajuda");
+        painelSul.add(botaoAjuda);
 
         // Configura ações dos botões
-
-        botaoSair.addActionListener(new ActionListener() {
+        botaoAjuda.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                System.exit(0);
+                infoTextArea.append("TESTE");
+            }
+        });
+
+        comandoTextField.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+                    String comando = comandoTextField.getText();
+                    processarComando(comando);
+                    comandoTextField.setText("");
+                }
             }
         });
 
@@ -85,7 +114,7 @@ public class DesempenhoJogoGUI extends JFrame implements InventarioDisciplinasGU
         });
         timer.start();
 
-        setSize(577, 578);
+        setSize(1700, 900);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
         setLocationRelativeTo(null);
@@ -105,6 +134,13 @@ public class DesempenhoJogoGUI extends JFrame implements InventarioDisciplinasGU
             timer.stop();
             System.out.println("Tempo esgotado! Fim do jogo.");
         }
+    }
+    /**
+     * Método privado para processar os comandos do jogo.
+     * @param comando Comando utilizado pelo jogador no chat.
+     */
+    private void processarComando(String comando) {
+        System.out.println("Comando inserido: " + comando);
     }
 
     /**
@@ -131,8 +167,6 @@ public class DesempenhoJogoGUI extends JFrame implements InventarioDisciplinasGU
         atualizarLabelTrabalhos();
     }
 }
-
-
 
 
 
